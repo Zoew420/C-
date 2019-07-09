68a280e39735a09bdd769010ad37134f5ed0f725
zhongjn <2389206378@qq.com>
2019-07-09 23:53:28 +0800

    优化帧数

--- view/game_drawParticles.h
+++ view/game_drawParticles.h
@@ -3,27 +3,31 @@
 #define PI 3.14159265358979323846
 
 inline void DrawCircle(float x, float y, float radius, int type) {
+    // 画圆有点降FPS，暂时改成方形
 	int count;
 	int sections = 200;
+    if (type == 1) glColor4f(1, 1, 0, 0);
+    if (type == 2) glColor4f(1, 1, 1, 0);
+    glRectf(x - radius, y - radius, x + radius, y + radius);
 
-	if (type == 1) {
-		glColor4f(1, 1, 0, 0);
-		glBegin(GL_TRIANGLE_FAN);
-		glVertex2f(x, y);
-		for (count = 0; count <= sections; count++)
-		{
-			glVertex2f(x + radius * cos(count * 2 * PI / sections), y + radius * sin(count * 2 * PI / sections));
-		}
-		glEnd();
-	}
-	else if (type == 2) {
-		glColor4f(1, 1, 1, 0);
-		glBegin(GL_TRIANGLE_FAN);
-		glVertex2f(x, y);
-		for (count = 0; count <= sections; count++)
-		{
-			glVertex2f(x + radius * cos(count * 2 * PI / sections), y + radius * sin(count * 2 * PI / sections));
-		}
-		glEnd();
-	}
+	//if (type == 1) {
+	//	glColor4f(1, 1, 0, 0);
+	//	glBegin(GL_TRIANGLE_FAN);
+	//	glVertex2f(x, y);
+	//	for (count = 0; count <= sections; count++)
+	//	{
+	//		glVertex2f(x + radius * cos(count * 2 * PI / sections), y + radius * sin(count * 2 * PI / sections));
+	//	}
+	//	glEnd();
+	//}
+	//else if (type == 2) {
+	//	glColor4f(1, 1, 1, 0);
+	//	glBegin(GL_TRIANGLE_FAN);
+	//	glVertex2f(x, y);
+	//	for (count = 0; count <= sections; count++)
+	//	{
+	//		glVertex2f(x + radius * cos(count * 2 * PI / sections), y + radius * sin(count * 2 * PI / sections));
+	//	}
+	//	glEnd();
+	//}
 }
