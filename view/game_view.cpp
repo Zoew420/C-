f30a097e22d5d65df279d7a34b96aab3776f49f4
zhongjn <2389206378@qq.com>
2019-07-13 00:26:57 +0800

    压强图正常显示

--- view/game_view.cpp
+++ view/game_view.cpp
@@ -37,69 +37,69 @@ Simflow::GameView::GameView()
     mode_pressure = false;
     on_frame_ready = make_shared<FrameReadyEventHandler>(this);
 
-	//错误回调函数
-	glfwSetErrorCallback(glfw_error_callback);
-	//初始化GLFW
-	if (!glfwInit()) exit(EXIT_FAILURE);
+    //错误回调函数
+    glfwSetErrorCallback(glfw_error_callback);
+    //初始化GLFW
+    if (!glfwInit()) exit(EXIT_FAILURE);
 
-	// GL 3.0 + GLSL 130
-	const char* glsl_version = "#version 130";
-	//配置GLFW
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+    // GL 3.0 + GLSL 130
+    const char* glsl_version = "#version 130";
+    //配置GLFW
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
 
-	//创建窗口
-	window = glfwCreateWindow(800, 512, "Simflow", NULL, NULL);
-	if (window == NULL)
-	{
-		glfwTerminate();
-		exit(EXIT_FAILURE);
-	}
-	//使用OpenGL API之前，必须设置OpenGL上下文环境
-	glfwMakeContextCurrent(window);
-	//设置交换间隔为1，vsync：要交换两个缓冲区之间的帧数
-	glfwSwapInterval(1);
+    //创建窗口
+    window = glfwCreateWindow(800, 512, "Simflow", NULL, NULL);
+    if (window == NULL)
+    {
+        glfwTerminate();
+        exit(EXIT_FAILURE);
+    }
+    //使用OpenGL API之前，必须设置OpenGL上下文环境
+    glfwMakeContextCurrent(window);
+    //设置交换间隔为1，vsync：要交换两个缓冲区之间的帧数
+    glfwSwapInterval(1);
 
-	//初始化 OpenGL Loader
+    //初始化 OpenGL Loader
 #if defined(IMGUI_IMPL_OPENGL_LOADER_GL3W)
-	bool err = gl3wInit() != 0;
+    bool err = gl3wInit() != 0;
 #elif defined(IMGUI_IMPL_OPENGL_LOADER_GLEW)
-	bool err = glewInit() != GLEW_OK;
+    bool err = glewInit() != GLEW_OK;
 #elif defined(IMGUI_IMPL_OPENGL_LOADER_GLAD)
-	bool err = gladLoadGL() == 0;
+    bool err = gladLoadGL() == 0;
 #else
-	bool err = false; // If you use IMGUI_IMPL_OPENGL_LOADER_CUSTOM, your loader is likely to requires some form of initialization.
+    bool err = false; // If you use IMGUI_IMPL_OPENGL_LOADER_CUSTOM, your loader is likely to requires some form of initialization.
 #endif
-	if (err)
-	{
-		fprintf(stderr, "Failed to initialize OpenGL loader!\n");
-		glfwTerminate();
-		exit(EXIT_FAILURE);
-	}
+    if (err)
+    {
+        fprintf(stderr, "Failed to initialize OpenGL loader!\n");
+        glfwTerminate();
+        exit(EXIT_FAILURE);
+    }
 
-	IMGUI_CHECKVERSION();
-	//设置默认字体
-	ImGui::CreateContext();
-	ImGuiIO& io = ImGui::GetIO(); (void)io;
+    IMGUI_CHECKVERSION();
+    //设置默认字体
+    ImGui::CreateContext();
+    ImGuiIO& io = ImGui::GetIO(); (void)io;
 
-	//设置ImGui模式为深色
-	ImGui::StyleColorsDark();
+    //设置ImGui模式为深色
+    ImGui::StyleColorsDark();
 
-	// Setup Platform/Renderer bindings
-	ImGui_ImplGlfw_InitForOpenGL(window, true);
-	ImGui_ImplOpenGL3_Init(glsl_version);
+    // Setup Platform/Renderer bindings
+    ImGui_ImplGlfw_InitForOpenGL(window, true);
+    ImGui_ImplOpenGL3_Init(glsl_version);
 
-	pressure_graph = make_unique<PressureGraphRenderer>();
+    pressure_graph = make_unique<PressureGraphRenderer>();
 }
 
 Simflow::GameView::~GameView()
 {
-	// Cleanup
-	ImGui_ImplOpenGL3_Shutdown();
-	ImGui_ImplGlfw_Shutdown();
-	ImGui::DestroyContext();
-	glfwDestroyWindow(window);
-	glfwTerminate();
+    // Cleanup
+    ImGui_ImplOpenGL3_Shutdown();
+    ImGui_ImplGlfw_Shutdown();
+    ImGui::DestroyContext();
+    glfwDestroyWindow(window);
+    glfwTerminate();
 }
 
 void Simflow::GameView::Handler(FrameData data)
@@ -107,9 +107,9 @@ void Simflow::GameView::Handler(FrameData data)
     if (mode_pressure)
     {
         ImGui::SetNextWindowPos(ImVec2(520, 120), ImGuiCond_Appearing);
-        ImGui::Begin("Pressure Graph", &mode_pressure, ImGuiWindowFlags_NoBackground | ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_AlwaysAutoResize);
+        ImGui::Begin("Pressure Graph", &mode_pressure, ImGuiWindowFlags_NoResize | ImGuiWindowFlags_AlwaysAutoResize);
         GLuint tex = pressure_graph->render_texture(data);
-        ImGui::Image((void*)tex, ImVec2(256, 256));
+        ImGui::Image((void*)tex, ImVec2(128, 128), ImVec2(0, 1), ImVec2(1, 0));
         ImGui::End();
     }
 
@@ -127,158 +127,158 @@ void Simflow::GameView::Handler(FrameData data)
 
 void Simflow::GameView::OnCreate()
 {
-	bool show_mouse_state = true;
-	bool show_menu = true;
+    bool show_mouse_state = true;
+    bool show_menu = true;
 
-	while (!glfwWindowShouldClose(window)) {
-		//渲染循环
-		glfwSwapBuffers(window);
-		glfwPollEvents();
-		//开启ImGui框架
-		ImGui_ImplOpenGL3_NewFrame();
-		ImGui_ImplGlfw_NewFrame();
-		ImGui::NewFrame();
+    while (!glfwWindowShouldClose(window)) {
+        //渲染循环
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+        //开启ImGui框架
+        ImGui_ImplOpenGL3_NewFrame();
+        ImGui_ImplGlfw_NewFrame();
+        ImGui::NewFrame();
 
-		glfwMakeContextCurrent(window);
-		glfwGetFramebufferSize(window, &display_w, &display_h);
-		glClear(GL_COLOR_BUFFER_BIT);
+        glfwMakeContextCurrent(window);
+        glfwGetFramebufferSize(window, &display_w, &display_h);
+        glClear(GL_COLOR_BUFFER_BIT);
 
-		ImGuiIO& io = ImGui::GetIO();
+        ImGuiIO& io = ImGui::GetIO();
 
-		if (show_menu) {
-			ImGui::SetNextWindowPos(ImVec2(0, 0), ImGuiCond_Appearing);
-			ImGui::Begin("Brush Function Choose:", &show_menu, ImGuiWindowFlags_NoMove | ImGuiWindowFlags_MenuBar | ImGuiWindowFlags_NoBackground | ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_AlwaysAutoResize);
-			if (ImGui::BeginMenuBar())
-			{
-				if (ImGui::BeginMenu("Mode Setting")) {
-					ImGui::CheckboxFlags("Paint", (unsigned int*)& mode_draw, 1); ImGui::SameLine(75);
-					if (mode_draw == true) {
-						mode_heat = false;
-						mode_pressure = false;
-					}
-					ImGui::CheckboxFlags("Temperature Graph", (unsigned int*)& mode_heat, 1); ImGui::SameLine(225);
-					if (mode_heat == true) {
-						mode_draw = false;
-						mode_pressure = false;
-					}
-					ImGui::CheckboxFlags("Pressure Graph", (unsigned int*)& mode_pressure, 1);
-					if (mode_pressure == true) {
-						mode_draw = false;
-						mode_heat = false;
-					}
-					ImGui::EndMenu();
-				}
-				ImGui::SameLine(110);
-				if (ImGui::BeginMenu("Brush Type Choose"))
-				{
-					ImGui::CheckboxFlags("Sand", (unsigned int*)& draw_sand, 1); ImGui::SameLine(150);
-					if (draw_sand == true)
-					{
-						draw_iron = false;
-						draw_water = false;
-					}
-					ImGui::CheckboxFlags("Iron", (unsigned int*)& draw_iron, 1); ImGui::SameLine(300);
-					if (draw_iron == true)
-					{
-						draw_sand = false;
-						draw_water = false;
-					}
-					ImGui::CheckboxFlags("Water", (unsigned int*)& draw_water, 1);
-					if (draw_water == true)
-					{
-						draw_iron = false;
-						draw_sand = false;
-					}
-					ImGui::EndMenu();
-				}
-				ImGui::SameLine(260);
-				if (ImGui::BeginMenu("Brush Size Choose"))
-				{
-					ImGui::CheckboxFlags("1 pix", (unsigned int*)& brush_1pix, 1); ImGui::SameLine(150);
-					if (brush_1pix == true) {
-						brush_5pix = false;
-						brush_10pix = false;
-					}
-					ImGui::CheckboxFlags("5 pix", (unsigned int*)& brush_5pix, 1); ImGui::SameLine(300);
-					if (brush_5pix == true) {
-						brush_1pix = false;
-						brush_10pix = false;
-					}
-					ImGui::CheckboxFlags("10 pix", (unsigned int*)& brush_10pix, 1);
-					if (brush_10pix == true) {
-						brush_1pix = false;
-						brush_5pix = false;
-					}
-					ImGui::EndMenu();
-				}
-				ImGui::SameLine(410);
-				if (ImGui::BeginMenu("Brush Function Choose"))
-				{
-					ImGui::CheckboxFlags("Painting Pen", (unsigned int*)& draw, 1); ImGui::SameLine(150);
-					if (draw == true) {
-						inc_heat = false;
-						dec_heat = false;
-					}
-					ImGui::CheckboxFlags("Increase Heat", (unsigned int*)& inc_heat, 1); ImGui::SameLine(300);
-					if (inc_heat == true) {
-						draw = false;
-						dec_heat = false;
-					}
-					ImGui::CheckboxFlags("Decrease Heat", (unsigned int*)& dec_heat, 1);
-					if (dec_heat == true) {
-						draw = false;
-						dec_heat = false;
-					}
-					ImGui::EndMenu();
-				}
-				ImGui::SameLine(745);
-				if (ImGui::BeginMenu("Exit")) exit(0);
-				ImGui::EndMenuBar();
-			}
-			ImGui::End();
-		}
+        if (show_menu) {
+            ImGui::SetNextWindowPos(ImVec2(0, 0), ImGuiCond_Appearing);
+            ImGui::Begin("Brush Function Choose:", &show_menu, ImGuiWindowFlags_NoMove | ImGuiWindowFlags_MenuBar | ImGuiWindowFlags_NoBackground | ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_AlwaysAutoResize);
+            if (ImGui::BeginMenuBar())
+            {
+                if (ImGui::BeginMenu("Mode Setting")) {
+                    ImGui::CheckboxFlags("Paint", (unsigned int*)& mode_draw, 1); ImGui::SameLine(75);
+                    if (mode_draw == true) {
+                        mode_heat = false;
+                        mode_pressure = false;
+                    }
+                    ImGui::CheckboxFlags("Temperature Graph", (unsigned int*)& mode_heat, 1); ImGui::SameLine(225);
+                    if (mode_heat == true) {
+                        mode_draw = false;
+                        mode_pressure = false;
+                    }
+                    ImGui::CheckboxFlags("Pressure Graph", (unsigned int*)& mode_pressure, 1);
+                    if (mode_pressure == true) {
+                        mode_draw = false;
+                        mode_heat = false;
+                    }
+                    ImGui::EndMenu();
+                }
+                ImGui::SameLine(110);
+                if (ImGui::BeginMenu("Brush Type Choose"))
+                {
+                    ImGui::CheckboxFlags("Sand", (unsigned int*)& draw_sand, 1); ImGui::SameLine(150);
+                    if (draw_sand == true)
+                    {
+                        draw_iron = false;
+                        draw_water = false;
+                    }
+                    ImGui::CheckboxFlags("Iron", (unsigned int*)& draw_iron, 1); ImGui::SameLine(300);
+                    if (draw_iron == true)
+                    {
+                        draw_sand = false;
+                        draw_water = false;
+                    }
+                    ImGui::CheckboxFlags("Water", (unsigned int*)& draw_water, 1);
+                    if (draw_water == true)
+                    {
+                        draw_iron = false;
+                        draw_sand = false;
+                    }
+                    ImGui::EndMenu();
+                }
+                ImGui::SameLine(260);
+                if (ImGui::BeginMenu("Brush Size Choose"))
+                {
+                    ImGui::CheckboxFlags("1 pix", (unsigned int*)& brush_1pix, 1); ImGui::SameLine(150);
+                    if (brush_1pix == true) {
+                        brush_5pix = false;
+                        brush_10pix = false;
+                    }
+                    ImGui::CheckboxFlags("5 pix", (unsigned int*)& brush_5pix, 1); ImGui::SameLine(300);
+                    if (brush_5pix == true) {
+                        brush_1pix = false;
+                        brush_10pix = false;
+                    }
+                    ImGui::CheckboxFlags("10 pix", (unsigned int*)& brush_10pix, 1);
+                    if (brush_10pix == true) {
+                        brush_1pix = false;
+                        brush_5pix = false;
+                    }
+                    ImGui::EndMenu();
+                }
+                ImGui::SameLine(410);
+                if (ImGui::BeginMenu("Brush Function Choose"))
+                {
+                    ImGui::CheckboxFlags("Painting Pen", (unsigned int*)& draw, 1); ImGui::SameLine(150);
+                    if (draw == true) {
+                        inc_heat = false;
+                        dec_heat = false;
+                    }
+                    ImGui::CheckboxFlags("Increase Heat", (unsigned int*)& inc_heat, 1); ImGui::SameLine(300);
+                    if (inc_heat == true) {
+                        draw = false;
+                        dec_heat = false;
+                    }
+                    ImGui::CheckboxFlags("Decrease Heat", (unsigned int*)& dec_heat, 1);
+                    if (dec_heat == true) {
+                        draw = false;
+                        dec_heat = false;
+                    }
+                    ImGui::EndMenu();
+                }
+                ImGui::SameLine(745);
+                if (ImGui::BeginMenu("Exit")) exit(0);
+                ImGui::EndMenuBar();
+            }
+            ImGui::End();
+        }
 
-		if (show_mouse_state) {
-			ImGui::SetNextWindowPos(ImVec2(475, 492), ImGuiCond_Appearing);
-			ImGui::Begin("Mouse state:", &show_mouse_state, ImGuiWindowFlags_NoBackground | ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_AlwaysAutoResize);
-			ImGui::Text("Mouse pos: (%g, %g)", io.MousePos.x, io.MousePos.y); ImGui::SameLine();
-			ImGui::Text("Click Last:");
-			if (io.MouseDownDuration[0] >= 0.0f)
-			{
-				ImGui::SameLine();
-				ImGui::Text("(%.02f secs)", io.MouseDownDuration[0]);
-			}
-			else {
-				ImGui::SameLine();
-				ImGui::Text("(0 secs)");
-			}
-			ImGui::End();
-		}
+        if (show_mouse_state) {
+            ImGui::SetNextWindowPos(ImVec2(475, 492), ImGuiCond_Appearing);
+            ImGui::Begin("Mouse state:", &show_mouse_state, ImGuiWindowFlags_NoBackground | ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_AlwaysAutoResize);
+            ImGui::Text("Mouse pos: (%g, %g)", io.MousePos.x, io.MousePos.y); ImGui::SameLine();
+            ImGui::Text("Click Last:");
+            if (io.MouseDownDuration[0] >= 0.0f)
+            {
+                ImGui::SameLine();
+                ImGui::Text("(%.02f secs)", io.MouseDownDuration[0]);
+            }
+            else {
+                ImGui::SameLine();
+                ImGui::Text("(0 secs)");
+            }
+            ImGui::End();
+        }
 
-		event_update.trigger();
+        event_update.trigger();
 
-		MouseClickEvent();
-		//绘制
-		ImGui::Render();
-		glViewport(0, 0, display_w, display_h);
-		ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
-	}
+        MouseClickEvent();
+        //绘制
+        ImGui::Render();
+        glViewport(0, 0, display_w, display_h);
+        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+    }
 }
 
 void Simflow::GameView::MouseClickEvent()
 {
-	ImGuiIO& io = ImGui::GetIO();
-	if (io.MouseDownDuration[0] >= 0.0f) {
-		GLfloat x, y;
-		x = io.MousePos.x;
-		y = io.MousePos.y;
-		if (y >= 50 && y <= 600) {
-			vec2 p;
-			p.x = x; p.y = y;
-			if (draw == true)  UpdataParticles(p);
-			else UpdataParticlesHeat(p);
-		}
-	}
+    ImGuiIO& io = ImGui::GetIO();
+    if (io.MouseDownDuration[0] >= 0.0f) {
+        GLfloat x, y;
+        x = io.MousePos.x;
+        y = io.MousePos.y;
+        if (y >= 50 && y <= 600) {
+            vec2 p;
+            p.x = x; p.y = y;
+            if (draw == true)  UpdataParticles(p);
+            else UpdataParticlesHeat(p);
+        }
+    }
 }
 
 void Simflow::GameView::UpdataParticles(const glm::vec2 & point)
--- view/game_view.h
+++ view/game_view.h
@@ -24,7 +24,7 @@ namespace Simflow {
 
     public:
         PressureGraphRenderer() {
-            create_framebuffer(&framebuffer, &framebuffer_tex, 512, 512);
+            create_framebuffer(&framebuffer, &framebuffer_tex, 128, 128);
         }
 
         // 返回纹理id
@@ -32,15 +32,17 @@ namespace Simflow {
             glBindFramebuffer(GL_FRAMEBUFFER, framebuffer);
             int m = data.pressure.width();
             int n = data.pressure.height();
-            reset_matrix(512, 512);
+            reset_matrix(128, 128);
             glClear(GL_COLOR_BUFFER_BIT);
-            for (int j = 0; j < n; j++) {
-                for (int i = 0; i < m; i++) {
+            glBegin(GL_POINTS);
+            for (int j = 0; j < n; j += 4) {
+                for (int i = 0; i < m; i += 4) {
                     float p = data.pressure[j][i];
-                    int x = i, y = j;
-                    DrawPressure(x, y, p);
-                }
+                    int x = i / 4, y = j / 4;
+                    set_color(p);
+                    glVertex2i(x, y);                }
             }
+            glEnd();
             glBindFramebuffer(GL_FRAMEBUFFER, 0);
             return framebuffer_tex;
         }
@@ -49,38 +51,38 @@ namespace Simflow {
 
     class GameView {
     protected:
-		/*压强图的纹理*/
+        /*压强图的纹理*/
         unique_ptr<PressureGraphRenderer> pressure_graph = nullptr;
 
-		/*绘制沙子与绘制固体的bool值*/
-		bool draw_sand;
-		bool draw_iron;
-		bool draw_water;
+        /*绘制沙子与绘制固体的bool值*/
+        bool draw_sand;
+        bool draw_iron;
+        bool draw_water;
 
-		/*画笔大小的选择*/
-		bool brush_1pix;
-		bool brush_5pix;
-		bool brush_10pix;
+        /*画笔大小的选择*/
+        bool brush_1pix;
+        bool brush_5pix;
+        bool brush_10pix;
 
-		/*画笔功能的选择*/
-		bool draw;
-		bool inc_heat;
-		bool dec_heat;
+        /*画笔功能的选择*/
+        bool draw;
+        bool inc_heat;
+        bool dec_heat;
 
-		/*窗口模式设定*/
-		bool mode_draw;
-		bool mode_heat;
-		bool mode_pressure;
+        /*窗口模式设定*/
+        bool mode_draw;
+        bool mode_heat;
+        bool mode_pressure;
 
-		/*更新ParticleBrush*/
-		void UpdataParticles(const vec2& point);
+        /*更新ParticleBrush*/
+        void UpdataParticles(const vec2& point);
 
-		/*更新HeatBrush*/
-		void UpdataParticlesHeat(const vec2& point);
+        /*更新HeatBrush*/
+        void UpdataParticlesHeat(const vec2& point);
 
     public:
-		/*GLF窗口*/
-		GLFWwindow* window;
+        /*GLF窗口*/
+        GLFWwindow* window;
 
         // 更新事件源（View通知ViewModel进行逻辑更新）
         EventSource<> event_update;
@@ -94,21 +96,21 @@ namespace Simflow {
         // 改变温度事件源
         EventSource<HeatBrush> event_change_heat;
 
-		//framedata处理事件指针
+        //framedata处理事件指针
         shared_ptr<EventHandler<FrameData>> on_frame_ready;
 
         GameView();
 
-		~GameView();
-		
-		//framedata处理事件
+        ~GameView();
+
+        //framedata处理事件
         void Handler(FrameData data);
 
-		//构造窗口环境
-		void OnCreate();
+        //构造窗口环境
+        void OnCreate();
 
-		//鼠标点击事件处理
-		void MouseClickEvent();
+        //鼠标点击事件处理
+        void MouseClickEvent();
     };
 }
 
